17369a7283358f0a661ef532e25e0c9d08670a46
divyasaireddy <divyasaireddy0910@gmail.com>
2024-09-23 23:40:02 +0530

    variables and data types

---
+++ task.js
@@ -0,0 +1,40 @@
+var details={
+    name:"divya",
+    age:21,
+    education:"btech graduate",
+}
+
+var objects={
+    name:"NarsiReddy",
+    age:45,
+    occupation:"Farmer",
+    salary:100000,
+    child1:{
+        name:"divya",age:21
+    },
+    child2:{
+        name:"navya",age:19
+    }
+}
+var array=[
+    name="Padma",
+    age=40,
+    ["occupation","Home maker","Farmer"],
+    children={
+      me:{
+        name:"divya",age:21,education:"graduate"
+      },
+      sister:{
+        name:"navya",age:19,education:"homeopathy"
+      }
+    } 
+]
+var arr=['name','roll.no']
+console.log(details);
+console.log(objects);
+console.log(array);
+
+
+
+
+
